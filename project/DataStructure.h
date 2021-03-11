6760b18810cdbf1d5a2a5c3fea902585a227c47b
SeHuibixiaoxing <wangzeyujiuyi@qq.com>
2021-03-11 09:18:19 +0800

    add

--- project/DataStructure.h
+++ project/DataStructure.h
@@ -37,6 +37,9 @@ struct VirtualMachine						//请求使用的虚拟机
 {
     int id;									//虚拟机ID
     int type;								//虚拟机型号
+
+    int nodeNum;                            //如果虚拟机在服务器中存储，nodeNum表示被部署到了哪个节点。双节点部署模式无效。
+    VirtualMachine* next = nullptr;         //如果虚拟机在服务器中存储，next表征链表
 };
 
 struct Server								//已经购买的、使用中的服务器
@@ -48,20 +51,125 @@ struct Server								//已经购买的、使用中的服务器
     int remainMemoryNodeA;					//服务器A结点剩余内存
     int remainMemoryNodeB;					//服务器B结点剩余内存
 
-    int open;								//服务器是否工作中
+    bool open;								//服务器是否工作中
     int cost;								//该服务器的累计成本
 
-    VirtualMachine* vmLinkHead = nullptr;	//该服务器中当前存在的虚拟机
+    VirtualMachine* virtualMachineLinkHead = nullptr;	//该服务器中当前存在的虚拟机
 
     static int severNum;					//已经购买的服务器数量
 
-    void addVirtualMachine(int type, int id)//添加虚拟机
-    {
+//    //检查type类型虚拟机是否能添加到当前服务器
+//    //core=0表示添加到A核，core=1表示添加到B核。若虚拟机为双节点部署,core无效
+//    void canAddVirtualMachine(int type, int id, int core = 0)
+//    {
+//
+//    }
 
+    //添加虚拟机到服务器;
+    //core=0表示添加到A核，core=1表示添加到B核。若虚拟机为双节点部署,core无效
+    //返回是否添加成功
+    bool addVirtualMachine(const VirtualMachineInformation& vmInfor, int type, int id, int core = 0)
+    {
+        if(vmInfor.isDoubleNode)
+        {
+            if(remainCoreNodeA < vmInfor.coreNumNode || remainCoreNodeB < vmInfor.coreNumNode)
+                return false;
+            if(remainMemoryNodeA < vmInfor.memorySizeNode || remainMemoryNodeB < vmInfor.memorySizeNode)
+                return false;
+
+            remainCoreNodeA -= vmInfor.coreNumNode, remainCoreNodeB -= vmInfor.coreNumNode;
+            remainMemoryNodeA -= vmInfor.memorySizeNode, remainMemoryNodeB -= vmInfor.memorySizeNode;
+
+            open = true;
+        }
+        else
+        {
+            if(core == 0)
+            {
+                if(remainCoreNodeA < vmInfor.coreNumNode)
+                    return false;
+                if(remainMemoryNodeA < vmInfor.memorySizeNode)
+                    return false;
+
+                remainCoreNodeA -= vmInfor.coreNumNode;
+                remainMemoryNodeA -= vmInfor.memorySizeNode;
+
+                open = true;
+            }
+            else if(core == 1)
+            {
+                if(remainCoreNodeB < vmInfor.coreNumNode)
+                    return false;
+                if(remainMemoryNodeB < vmInfor.memorySizeNode)
+                    return false;
+
+                remainCoreNodeB -= vmInfor.coreNumNode;
+                remainMemoryNodeB -= vmInfor.memorySizeNode;
+
+                open = true;
+            }
+            else return false;
+        }
+
+        VirtualMachine* now = new VirtualMachine();
+        now->id = id;
+        now->type = type;
+        now->next = virtualMachineLinkHead;
+        virtualMachineLinkHead = now;
+
+        return true;
     }
-    void delVirtualMachine(int id)			//删除虚拟机
-    {
 
+    //删除虚拟机
+    //若不存在ID为id的虚拟机，返回false
+    bool delVirtualMachine(const VirtualMachineInformation& vmInfor, int id)
+    {
+        VirtualMachine* now = virtualMachineLinkHead;
+        while(now != nullptr)
+        {
+            if(now->id == id)
+            {
+                if(vmInfor.isDoubleNode)
+                {
+                    remainCoreNodeA += vmInfor.coreNumNode, remainCoreNodeB += vmInfor.coreNumNode;
+                    remainMemoryNodeA += vmInfor.memorySizeNode, remainMemoryNodeB += vmInfor.memorySizeNode;
+                }
+                else
+                {
+                    if(core == 0)
+                    {
+                        if(remainCoreNodeA < vmInfor.coreNumNode)
+                            return false;
+                        if(remainMemoryNodeA < vmInfor.memorySizeNode)
+                            return false;
+
+                        remainCoreNodeA -= vmInfor.coreNumNode;
+                        remainMemoryNodeA -= vmInfor.memorySizeNode;
+
+                        open = true;
+                    }
+                    else if(core == 1)
+                    {
+                        if(remainCoreNodeB < vmInfor.coreNumNode)
+                            return false;
+                        if(remainMemoryNodeB < vmInfor.memorySizeNode)
+                            return false;
+
+                        remainCoreNodeB -= vmInfor.coreNumNode;
+                        remainMemoryNodeB -= vmInfor.memorySizeNode;
+
+                        open = true;
+                    }
+                }
+
+
+                if(virtualMachineLinkHead == nullptr)
+                    open = false;
+                return true;
+            }
+            else now = now->next;
+        }
+        return false;
     }
 };
 #endif //PROJECT_DATASTRUCTURE_H
